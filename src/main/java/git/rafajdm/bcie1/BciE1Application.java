02febdb39194c07bf6ddfdfc8954e91632989a88
Rafael Jose D <diazrafael18@gmail.com>
2023-06-06 12:16:55 -0400

    Commit inicial

---
+++ src/main/java/git/rafajdm/bcie1/BciE1Application.java
@@ -0,0 +1,13 @@
+package git.rafajdm.bcie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciE1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BciE1Application.class, args);
+	}
+
+}
---
+++ src/test/java/git/rafajdm/bcie1/BciE1ApplicationTests.java
@@ -0,0 +1,13 @@
+package git.rafajdm.bcie1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciE1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
